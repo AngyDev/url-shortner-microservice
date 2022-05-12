976ae86858e474f71a550e6d04cc8e001b205dc0
Angela Busato <busato.angela@gmail.com>
2022-05-12 19:21:13 +0200

    feat: move client connection to index file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { router } from "./route.js";
+import { promisify } from "util";
+import client from "./redis-client.js";
 
 const app = express();
+const startServer = promisify(app.listen);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -12,8 +16,14 @@ app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// client.on("error", (err) => {
+//   throw new Error(err.message);
+// });
+
+await client.connect();
+
+await startServer(PORT);
+
+console.log(`Server listening on port ${PORT}`);
 
 export default { app };
--- redis-client.js
+++ redis-client.js
@@ -2,9 +2,9 @@ import { createClient } from "redis";
 
 const client = createClient();
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+export default client;
 
-await client.connect();
+
 
 async function postUrl(url, shortUrl) {
   // Sets the key, value
